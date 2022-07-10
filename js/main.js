062e61793caf70b689fcea5ec6afd48780295ee0
aldiui <aldijaya280902@gmail.com>
2022-07-10 14:32:33 +0700

    undanganui

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// owl carousel start
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 15,
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 500,
+    responsive: {
+      0: {
+        items: 1,
+        dots: false,
+      },
+      600: {
+        items: 2,
+        dots: false,
+      },
+      1000: {
+        items: 3,
+        dots: false,
+      },
+    },
+  });
+});
+// owl carousel end
