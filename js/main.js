12585587d2037167fb62a1ab231aa5c4b5740f05
aldiui <aldijaya280902@gmail.com>
2022-07-10 20:52:07 +0700

    komit

--- js/main.js
+++ js/main.js
@@ -24,3 +24,35 @@ $(document).ready(function () {
   });
 });
 // owl carousel end
+
+// copy start
+const rek1 = document.getElementById("rek1");
+const salin1 = document.getElementById("salin1");
+
+salin1.onclick = () => {
+  rek1.select(); // Selects the text inside the input
+  document.execCommand("copy"); // Simply copies the selected text to clipboard
+  Swal.fire({
+    //displays a pop up with sweetalert
+    icon: "success",
+    title: "No. Rekening Berhasil di Salin",
+    showConfirmButton: false,
+    timer: 1000,
+  });
+};
+
+const rek2 = document.getElementById("rek2");
+const salin2 = document.getElementById("salin2");
+
+salin2.onclick = () => {
+  rek2.select(); // Selects the text inside the input
+  document.execCommand("copy"); // Simply copies the selected text to clipboard
+  Swal.fire({
+    //displays a pop up with sweetalert
+    icon: "success",
+    title: "No. Rekening Berhasil di Salin",
+    showConfirmButton: false,
+    timer: 1000,
+  });
+};
+// copy end
