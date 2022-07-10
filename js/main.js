92f2d93560fbb1077c7fb01936699b2e638e38ed
aldiui <aldijaya280902@gmail.com>
2022-07-10 21:25:47 +0700

    komitk123

--- js/main.js
+++ js/main.js
@@ -58,14 +58,14 @@ salin2.onclick = () => {
 // copy end
 
 // waktu start
-var countDownDate = new Date("Aug 1, 2022 09:00:00").getTime();
-var x = setInterval(function () {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+const countDownDate = new Date("Aug 1, 2022 09:00:00").getTime();
+const x = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
   document.getElementById("hari").innerHTML = days;
   document.getElementById("jam").innerHTML = hours;
   document.getElementById("menit").innerHTML = minutes;
