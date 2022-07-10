b44466073648f223fbaa4530d2547745f466073c
aldiui <aldijaya280902@gmail.com>
2022-07-10 21:03:03 +0700

    komit1

--- js/main.js
+++ js/main.js
@@ -56,3 +56,23 @@ salin2.onclick = () => {
   });
 };
 // copy end
+
+// waktu start
+var countDownDate = new Date("Aug 1, 2022 09:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("hari").innerHTML = days;
+  document.getElementById("jam").innerHTML = hours;
+  document.getElementById("menit").innerHTML = minutes;
+  document.getElementById("detik").innerHTML = seconds;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Carasingkat").innerHTML = "EXPIRED";
+  }
+}, 1000);
+// waktu end
